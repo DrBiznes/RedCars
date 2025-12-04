8eb684d54fc3d3a7e2dc34dd51ed7ae899999c2a
DrBzines <jamesfemino@gmail.com>
2025-12-03 16:42:32 -0800

    finished

--- src/pages/Feedback.tsx
+++ src/pages/Feedback.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+
 import { Button } from '@/components/ui/button';
 import { PacificElectricLoader } from '@/components/ui/PacificElectricLoader';
 import { Mail, ArrowLeft } from 'lucide-react';
